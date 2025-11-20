6710c18498bf6ddd870af405c947d8ad17bd80bd
My Content <safonov.e84@gmail.com>
2025-11-20 21:13:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+﻿
+function openIdeas(){ window.location.href = "ideas.html"; }
+function openGenerator(){ window.location.href = "generator.html"; }
+function openReels(){ window.location.href = "reels.html"; }
+
+// small helper to create element from html string
+function html(str){ const t = document.createElement("template"); t.innerHTML = str.trim(); return t.content.firstChild;}
+
